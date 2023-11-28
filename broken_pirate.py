78f58d6ecb5c3a00ff31f7e2ccf2be3a139d7e6e
Shafaq <shafaqmandha@gmail.com>
2023-11-28 07:45:11 +0400

    Final version - task 2

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,3 +1,9 @@
+'''
+    Name: Shafaq Mandha 
+    MISIS: M00914211
+    Module: PDE1130
+    SOB no. 31
+'''
 greeting = input("Hello, possible pirate! What's the password?") # Added a double quote to end the input prompt.
 if greeting == "Arrr!":
 	print("Go away, pirate.")
