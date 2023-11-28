7ccbae3ef7aa904d0ad01f80672c698c5031757b
Shafaq <shafaqmandha@gmail.com>
2023-11-28 07:41:14 +0400

    Final version - task 1

--- broken_grades.py
+++ broken_grades.py
@@ -1,3 +1,9 @@
+'''
+    Name: Shafaq Mandha 
+    MISIS: M00914211
+    Module: PDE1130
+    SOB no. 31
+'''
 # Calculating Grades (ok, let me think about this one)
 
 # Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
